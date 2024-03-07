c8a7fe7c10740a0e6c5337f4923b21d573143268
rstotler <rstotler87@gmail.com>
2024-03-07 16:16:17 -0500

    X Velocity & Collision Detection

--- core/src/com/jbs/platformerengine/gamedata/player/Player.java
+++ core/src/com/jbs/platformerengine/gamedata/player/Player.java
@@ -16,6 +16,8 @@ public class Player {
     public Rect spriteArea;
 
     PointF velocity;
+    int moveSpeed;
+
     boolean jumpCheck;
     boolean jumpButtonPressedCheck;
     int jumpTimerMax;
@@ -27,6 +29,8 @@ public class Player {
         spriteArea = new Rect(100, 250, 16, 48);
 
         velocity = new PointF(0, 0);
+        moveSpeed = 3;
+
         jumpCheck = false;
         jumpButtonPressedCheck = false;
         jumpTimerMax = 10;
@@ -40,14 +44,18 @@ public class Player {
     }
 
     public void updateInput(Keyboard keyboard) {
-        int moveSpeed = 5;
-        
+
+        // Sideways Velocity //
         if(keyboard.left && !keyboard.right) {
-            spriteArea.x -= moveSpeed;
+            velocity.x = -moveSpeed;
         } else if(!keyboard.left && keyboard.right) {
-            spriteArea.x += moveSpeed;
+            velocity.x = moveSpeed;
+        }
+        if(keyboard.lastUp != null && (keyboard.lastUp.equals("Left") || keyboard.lastUp.equals("Right"))) {
+            velocity.x = 0;
         }
 
+        // Jump Velocity //
         if(keyboard.up) {
             if(!jumpCheck && !jumpButtonPressedCheck) {
                 velocity.y = 10;
@@ -68,6 +76,13 @@ public class Player {
     }
 
     public void updateCollisions(ScreenChunk[][] screenChunks) {
+
+        // Sideways Movement //
+        if(velocity.x != 0) {
+            spriteArea.x += velocity.x;
+        }
+
+        // Gravity //
         if(velocity.y > maxFallVelocity) {
             if(jumpTimer == jumpTimerMax) {
                 velocity.y -= .7;
@@ -76,13 +91,41 @@ public class Player {
             }
         }
 
-        spriteArea.y += velocity.y;
-
-        int chunkX = spriteArea.x / Gdx.graphics.getWidth();
+        // X Collision Detection //
+        int chunkX = (spriteArea.x - (spriteArea.width / 2)) / Gdx.graphics.getWidth();
         int chunkY = spriteArea.y / Gdx.graphics.getHeight();
         ScreenChunk targetChunk = screenChunks[chunkX][chunkY];
 
-        int targetTileX = (spriteArea.x % Gdx.graphics.getWidth()) / 16;
+        if(velocity.x < 0) {
+            int targetTileX = ((spriteArea.x % Gdx.graphics.getWidth()) - (spriteArea.width / 2)) / 16;
+            int targetTileY = (spriteArea.y % Gdx.graphics.getHeight()) / 16;
+            Tile targetTile = targetChunk.tiles[targetTileX][targetTileY];
+
+            if(targetTile != null) {
+                spriteArea.x = (targetChunk.location.x * Gdx.graphics.getWidth()) + (targetTileX * 16) + (spriteArea.width / 2) + 16;
+                velocity.x = 0;
+            }
+        } else if(velocity.x > 0) {
+            int targetTileX = ((spriteArea.x % Gdx.graphics.getWidth()) + (spriteArea.width / 2)) / 16;
+            int targetTileY = (spriteArea.y % Gdx.graphics.getHeight()) / 16;
+            Tile targetTile = targetChunk.tiles[targetTileX][targetTileY];
+
+            if(targetTile != null) {
+                spriteArea.x = (targetChunk.location.x * Gdx.graphics.getWidth()) + (targetTileX * 16) - (spriteArea.width / 2);
+                velocity.x = 0;
+            }
+        }
+
+        // Up/Down Movement //
+        spriteArea.y += velocity.y;
+
+        // Y Collision Detection (Left Side) //
+        boolean yCollisionCheck = false;
+        chunkX = (spriteArea.x - (spriteArea.width / 2)) / Gdx.graphics.getWidth();
+        chunkY = spriteArea.y / Gdx.graphics.getHeight();
+        targetChunk = screenChunks[chunkX][chunkY];
+
+        int targetTileX = ((spriteArea.x - (spriteArea.width / 2)) % Gdx.graphics.getWidth()) / 16;
         int targetTileY = (spriteArea.y % Gdx.graphics.getHeight()) / 16;
         Tile targetTile = targetChunk.tiles[targetTileX][targetTileY];
 
@@ -91,6 +134,25 @@ public class Player {
             velocity.y = 0;
             jumpCheck = false;
             jumpTimer = jumpTimerMax;
+            yCollisionCheck = true;
+        }
+
+        // Y Collision Detection (Right Side) //
+        if(!yCollisionCheck) {
+            chunkX = (spriteArea.x + (spriteArea.width / 2) - 1) / Gdx.graphics.getWidth();
+            chunkY = spriteArea.y / Gdx.graphics.getHeight();
+            targetChunk = screenChunks[chunkX][chunkY];
+    
+            targetTileX = ((spriteArea.x + (spriteArea.width / 2) - 1) % Gdx.graphics.getWidth()) / 16;
+            targetTileY = (spriteArea.y % Gdx.graphics.getHeight()) / 16;
+            targetTile = targetChunk.tiles[targetTileX][targetTileY];
+    
+            if(targetTile != null) {
+                spriteArea.y = (targetChunk.location.y * Gdx.graphics.getHeight()) + (targetTileY * 16) + 16;
+                velocity.y = 0;
+                jumpCheck = false;
+                jumpTimer = jumpTimerMax;
+            }
         }
     }
 
